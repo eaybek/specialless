d65d2112ba84ac068c87f60ae5524779e0d6b805
Erdem Aybek <eaybek@gmail.com>
2019-09-20 21:29:14 +0300

    initial functionality

--- sample/sample.py
+++ sample/sample.py
@@ -1,5 +1,13 @@
-from specialless.specialless import Specialless
+import os
+from specialless.specialless import Revertable
+
+my_special_string = ">£#$½¾{()€ª¶[]]}`````''''''$$$$$$"
+print(my_special_string)
+not_so_special = Revertable.convert(my_special_string)
+print(not_so_special)
+special_again = Revertable.revert(not_so_special)
+print(special_again)
 
 
-class Specialless(object):
-    pass
+# example use case
+print(Revertable.convert(os.path.abspath(os.path.curdir)))
