5abab91925355f66b5dde273b7a9f0f1a4ccf079
Erdem Aybek <eaybek@gmail.com>
2019-09-20 10:07:28 +0300

    read the docs fix confpy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Specialless'
-copyright = '2019, Erdem Aybek'
-author = 'Erdem Aybek'
+project = "Specialless"
+copyright = "2019, Erdem Aybek"
+author = "Erdem Aybek"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,12 +27,10 @@ author = 'Erdem Aybek'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -45,12 +43,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
-# -- Extension configuration -------------------------------------------------master_doc = "index"
+# -- Extension configuration -------------------------------------------------
+master_doc = "index"
